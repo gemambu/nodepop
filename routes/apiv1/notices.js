2e84d40778464a8e125519f2edd692c43fa6e178
Gema <gemambu@gmail.com>
2017-05-15 12:54:13 +0200

    correct filter by price

--- routes/apiv1/notices.js
+++ routes/apiv1/notices.js
@@ -11,17 +11,15 @@ const Notice = require('../../models/Notice');
 
 /* GET /apiv1/agentes */
 router.get('/', function(req, res, next) {
-    console.log('query: ', req.query);
-    //const name = req.query.name;
-    const name = new RegExp('^'+ req.query.nombre, "i");
+    const name = new RegExp('^'+ req.query.nombre, 'i');
     const sale = req.query.venta;
     const price = req.query.precio;
-    const photo = req.query.photo;
     const tag = req.query.tag;
     const limit = parseInt(req.query.limit);
-    const skip = parseInt(req.query.skip);
+    const skip = parseInt(req.query.start);
     const fields = req.query.fields;
     const sort = req.query.sort;
+    const includeTotal = req.query.includeTotal ? req.query.includeTotal : 'true';
 
 
     // creamos el filtro vacio
@@ -36,18 +34,18 @@ router.get('/', function(req, res, next) {
         filter.sale = sale.toLowerCase();
     }
     if(price){
-
-
-        if(price.indexOf('-' === -1)){ // the price filter is exact price
+        console.log(price.indexOf('-'));
+        console.log(price.length);
+        if(price.indexOf('-') === -1){ // the price filter is exact price
             filter.price = parseInt(price);
         }
-        if(price.indexOf('-') === 0 ){ // the price filter is less than
-            var limitP = price.substring(1, price.length);
-            filter.price = { '$lte': limitP };
+        if(price.indexOf('-') === 0){ // the price filter is less than
+            var limitLte = price.substring(1, price.length);
+            filter.price = { '$lte': limitLte };
         }
-        if(price.indexOf('-' === price.length)){ // the price filter is greater than
-            var limitP = price.substring(0, price.length - 1);
-            filter.price = { '$gte': limitP };
+        if(price.indexOf('-') === price.length -1){ // the price filter is greater than
+            var limitGte = price.substring(0, price.length - 1);
+            filter.price = { '$gte': limitGte };
         }
         if(price.indexOf('-') > 0 && price.indexOf('-') < price.length -1 ){ // the price filter has two limits
             var medium = price.indexOf('-');
@@ -55,45 +53,21 @@ router.get('/', function(req, res, next) {
             var limitLess = price.substring(medium + 1, price.length);            
             filter.price = { '$gte': LimitGreater , '$lte': limitLess  };
         }
-
-        console.log(filter);
-
     }
+     console.log(filter);
 
     Notice.list(filter, limit, skip, fields, sort, (err, notices) => {
         if(err){
             next(err); // le decimos a express que devuelva el error
             return;
         }
-
-        res.json({success: true, result: notices});
-
-    });
-
-});
-
-
-// POST /apiv1/notices
-router.post('/', (req, res, next) => {
-    console.log(req.body);
-
-    //TODO: validate the info, check if everything is OK
-    // Las validaciones de minimos y maximos, usar mongoose.
-
-    // creamos un objecto de tipo Agente con la peticion mandada
-    const notice = new Notice(req.body);
-
-    // lo guardamos en la BD
-    notice.save((err, newNotice) => {
-        if (err){
-            next(err);
-            return;
+        if(includeTotal === 'true'){
+            res.json({success: true, number: notices.length, result: notices});
+        } else {
+            res.json({success: true, result: notices});    
         }
-        res.json({success: true, result: newNotice});
+
     });
-
-
-
 });
 
 module.exports = router;
